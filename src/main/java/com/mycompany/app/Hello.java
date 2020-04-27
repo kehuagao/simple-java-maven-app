af0554632ebd421b44f349eec75f2fbb0456c367
kehuagao <khmantis@126.com>
2020-04-27 16:52:20 +0800

    2020.4.27

---
+++ src/main/java/com/mycompany/app/Hello.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ * kehua
+ */
+public class App
+{
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+
+}
